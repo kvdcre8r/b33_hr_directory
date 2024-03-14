d27163e385e042c6e674a6fe0e39bfc69699a036
kvdcre8r <kvdcre8r@gmail.com>
2024-03-14 10:55:03 -0500

    completed workshop

---
+++ server.js
@@ -0,0 +1,113 @@
+// imports
+const express = require("express");
+const pg = require("pg");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// static routes
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/acme_hr_directory_db"
+);
+
+// parse the body and log incoming requests
+app.use(express.json());
+app.use(require("morgan")("dev"));
+
+// app routes
+app.get("/api/employees", async (req, res, next) => {
+  try {
+    const SQL = `
+        SELECT * from employees;
+      `;
+    const result = await client.query(SQL);
+    res.send(result.rows);
+  } catch (ex) {
+    next(ex);
+  }
+});
+app.get("/api/departments", async (req, res, next) => {
+  try {
+    const SQL = `
+        SELECT * from departments;
+      `;
+    const result = await client.query(SQL);
+    res.send(result.rows);
+  } catch (ex) {
+    next(ex);
+  }
+});
+app.post("/api/employees", async (req, res, next) => {
+  try {
+    const SQL = `INSERT INTO employees(name, department_id)
+        VALUES ($1, (SELECT id FROM departments WHERE name=$2))
+        RETURNING *`;
+    const result = await client.query(SQL, [
+      req.body.name,
+      req.body.departmentName,
+    ]);
+    res.send(result.rows[0]);
+  } catch (ex) {
+    next(ex);
+  }
+});
+app.delete("/api/employees/:id", async (req, res, next) => {
+  try {
+    const SQL = `
+        DELETE FROM employees
+        WHERE id = $1;
+      `;
+    const response = await client.query(SQL, [req.params.id]);
+    res.sendStatus(204);
+  } catch (ex) {
+    next(ex);
+  }
+});
+app.put("/api/employees/:id", async (req, res, next) => {
+  try {
+    const SQL = `UPDATE employees
+        SET name = $1, department_id = (SELECT id FROM departments WHERE name=$2), updated_at=now()
+        WHERE id = $3 RETURNING *`;
+    const result = await client.query(SQL, [
+      req.body.name,
+      req.body.departmentName,
+      req.params.id,
+    ]);
+    res.send(result.rows[0]);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+// init function
+const init = async () => {
+  await client.connect();
+  console.log("connected to database");
+  let SQL = `DROP TABLE IF EXISTS employees;
+    DROP TABLE IF EXISTS departments;
+    CREATE TABLE departments(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL);
+    CREATE TABLE employees(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT now(),
+        updated_at TIMESTAMP DEFAULT now(),
+        department_id INTEGER REFERENCES departments(id) NOT NULL);`;
+  await client.query(SQL);
+  console.log("tables created");
+
+  SQL = ` INSERT INTO departments(name) VALUES('Human Resources');
+    INSERT INTO departments(name) VALUES('Accounting');
+    INSERT INTO departments(name) VALUES('Information Technology');
+    INSERT INTO employees(name, department_id) VALUES('Steve', (SELECT id FROM departments WHERE name='Human Resources'));
+    INSERT INTO employees(name, department_id) VALUES('Clint', (SELECT id FROM departments WHERE name='Human Resources'));
+    INSERT INTO employees(name, department_id) VALUES('Natasha', (SELECT id FROM departments WHERE name='Accounting'));
+    INSERT INTO employees(name, department_id) VALUES('Tony', (SELECT id FROM departments WHERE name='Information Technology'));
+    INSERT INTO employees(name, department_id) VALUES('Bruce', (SELECT id FROM departments WHERE name='Information Technology'));`;
+  await client.query(SQL);
+  console.log("data seeded");
+
+  app.listen(port, () => console.log(`listening on port ${port}`));
+};
+
+init();
